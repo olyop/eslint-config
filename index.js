ce72aa0466a122da603777751d594482403bcaaa
Oliver <oliver.plummer@outlook.com>
2021-01-29 13:51:32 +1100

    Update

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ module.exports = {
 	parser: "@typescript-eslint/parser",
 	extends: [
 		"airbnb-typescript",
-		"airbnb/hooks",
 		"plugin:@typescript-eslint/eslint-recommended",
 		"plugin:@typescript-eslint/recommended",
 		"plugin:@typescript-eslint/recommended-requiring-type-checking",
